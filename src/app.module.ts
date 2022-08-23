e1d8347ed9e155a78e593b78fac2d478678b534b
Cimorexave <sadeq.s502@gmail.com>
2022-08-23 13:08:20 +0430

    prisma module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './Auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
