097795f7d2e20f010ff7f40dd4591fff6857a60a
Cimorexave <sadeq.s502@gmail.com>
2022-08-23 13:17:20 +0430

    prisma db connection setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import config from 'config';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: config.DATABASE_URL
+                }
+            }
+        })
+    }
+}
