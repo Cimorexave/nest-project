bf5cc2b34201e45f722e65c3cd97730ad64117ab
unknown <sadeq.s502@gmail.com>
2022-08-27 12:04:33 +0430

    added request typing to custom decorator

--- src/Auth/decorator/get-user.decorator.ts
+++ src/Auth/decorator/get-user.decorator.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+    const request: Express.Request = ctx.switchToHttp().getRequest();
     return request.user;
   },
 );
